36d6266768a195fb08e98c18d9677754a9e729a9
Chris McLearnon <chrismclearnon@icloud.com>
2023-01-23 19:03:59 +0000

    Update playground calls

--- BinarySearchTree/BinarySearchTree.playground/Contents.swift
+++ BinarySearchTree/BinarySearchTree.playground/Contents.swift
@@ -1,14 +1,17 @@
 import Cocoa
 
-var tree: BinarySearchTree<Int> = .empty
-tree.insert(newValue: 7)
-tree.insert(newValue: 10)
+var tree = BinarySearchTree.leaf(7)
 tree.insert(newValue: 2)
+tree.insert(newValue: 10)
 tree.insert(newValue: 1)
 tree.insert(newValue: 5)
 tree.insert(newValue: 9)
 
 print("====Full Binary Tree====\n\(tree)")
+
+print("\n\n====Searching====")
+print(tree.search(forValue: 5) ?? "Value not found")
+print(tree.search(forValue: 11) ?? "Value not found")
 
 print("\n\n====In Order====")
 tree.traverseInOrder { value in
@@ -25,10 +28,3 @@ print("\n\n====Post Order====")
 tree.traversePostOrder { value in
     print(value)
 }
-
-print("\n\n====Searching====")
-print(tree.search(forValue: 5) ?? "Value not found")
-print(tree.search(forValue: 11) ?? "Value not found")
-
-
-
